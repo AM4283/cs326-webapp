4243d92d5410cdf3b75455aad3ce021733eb6379
Albi M. <albimarini4283@gmail.com>
2024-04-01 20:18:56 -0400

    Added images, changed css, added webfont, ran prettier on current code

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -1,77 +1,77 @@
 function readMore() {
-    const headers = document.querySelectorAll('main > section > h2');
-    headers.forEach(header => {
-        header.addEventListener('click', function () {
-            const paragraph = this.nextElementSibling;
-            if (paragraph.style.display === 'none') {
-                paragraph.style.display = 'block';
-            } else {
-                paragraph.style.display = 'none';
-            }
-        });
+  const headers = document.querySelectorAll("main > section > h2");
+  headers.forEach((header) => {
+    header.addEventListener("click", function () {
+      const paragraph = this.nextElementSibling;
+      if (paragraph.style.display === "none") {
+        paragraph.style.display = "block";
+      } else {
+        paragraph.style.display = "none";
+      }
     });
+  });
 }
 function imageShowcase() {
-    const img_src = [
-        "https://www.pngall.com/wp-content/uploads/1/Coat-Free-PNG.png",
-        "https://www.pngall.com/wp-content/uploads/1/Coat-Free-PNG.png",
-        "https://i.pinimg.com/474x/51/7b/c2/517bc28512e3d3ba53a286487a32a8e4.jpg",
-        "https://www.pngall.com/wp-content/uploads/1/Coat-Free-PNG.png",
-        "https://i.pinimg.com/474x/51/7b/c2/517bc28512e3d3ba53a286487a32a8e4.jpg",
-        "https://www.pngall.com/wp-content/uploads/1/Coat-Free-PNG.png",
-        "https://www.pngall.com/wp-content/uploads/1/Coat-Free-PNG.png",
-        "https://www.pngall.com/wp-content/uploads/1/Coat-Free-PNG.png",
-    ];
+  const img_src = [
+    "/img/burberryhat.webp",
+    "/img/lvcoat.png",
+    "/img/nikefleece.png",
+    "/img/no9tee.png",
+    "/img/scarf.png",
+  ];
 
-    const showcase = document.getElementById('image-showcase');
-    const scroll_left = document.getElementById('scroll-left');
-    const scroll_right = document.getElementById('scroll-right');
+  const showcase = document.getElementById("image-showcase");
+  const scroll_left = document.getElementById("scroll-left");
+  const scroll_right = document.getElementById("scroll-right");
 
-    for (let i = 0; i < 3; i++) {
-           const first_img = img_src[i];
-           const last_img = img_src[img_src.length - 1 - i];
-           
-           const firstClone = document.createElement('img');
-           firstClone.src = first_img;
-           firstClone.alt = "showcase";
-           firstClone.classList.add("clone");
-    
-           const lastClone = document.createElement('img');
-           lastClone.src = last_img;
-           lastClone.alt = "showcase";
-           lastClone.classList.add("clone"); // class for clone elements
-    
-           showcase.appendChild(firstClone); // first clones at the end
-           showcase.insertBefore(lastClone, showcase.firstChild); // last clone before first child
+  for (let i = 0; i < 3; i++) {
+    const first_img = img_src[i];
+    const last_img = img_src[img_src.length - 1 - i];
+
+    const firstClone = document.createElement("img");
+    firstClone.src = first_img;
+    firstClone.alt = "showcase";
+    firstClone.classList.add("clone");
+
+    const lastClone = document.createElement("img");
+    lastClone.src = last_img;
+    lastClone.alt = "showcase";
+    lastClone.classList.add("clone"); // class for clone elements
+
+    showcase.appendChild(firstClone); // first clones at the end
+    showcase.insertBefore(lastClone, showcase.firstChild); // last clone before first child
+  }
+  img_src.forEach((src) => {
+    const img = document.createElement("img");
+    img.src = src;
+    img.alt = "showcase";
+    showcase.appendChild(img);
+  });
+  let scrollAmount = 0;
+
+  function scroll(direction) {
+    const width = showcase.clientWidth / 3; // width of each image card
+    // const width = 216*3;
+    if (direction === "left") {
+      scrollAmount -= width;
+      if (scrollAmount < 0) {
+        // loop to end if > start
+        scrollAmount = showcase.scrollWidth - showcase.parentNode.offsetWidth;
+      }
+    } else {
+      scrollAmount += width;
+      if (
+        scrollAmount >=
+        showcase.scrollWidth - showcase.parentNode.offsetWidth
+      ) {
+        // loop to start if > end
+        scrollAmount = 0;
+      }
     }
-    img_src.forEach(src => {
-        const img = document.createElement('img');
-        img.src = src;
-        img.alt = "showcase";
-        showcase.appendChild(img);
-    });
-    let scrollAmount = 0;
-
-    function scroll(direction) {
-        const width = showcase.clientWidth / 3; // width of each image card
-        // const width = 216*3;
-        if (direction === "left") {
-            scrollAmount -= width;
-            if (scrollAmount < 0) {
-                // loop to end if > start
-                scrollAmount = showcase.scrollWidth - showcase.parentNode.offsetWidth;
-            }
-        } else {
-            scrollAmount += width;
-            if (scrollAmount >= showcase.scrollWidth - showcase.parentNode.offsetWidth) {
-                // loop to start if > end
-                scrollAmount = 0;
-            }
-        }
-        showcase.style.transform = `translateX(-${scrollAmount}px)`;
-    }
-    scroll_left.addEventListener('click', () => scroll("left"));
-    scroll_right.addEventListener('click', () => scroll("right"));
+    showcase.style.transform = `translateX(-${scrollAmount}px)`;
+  }
+  scroll_left.addEventListener("click", () => scroll("left"));
+  scroll_right.addEventListener("click", () => scroll("right"));
 }
-document.addEventListener('DOMContentLoaded',readMore);
-document.addEventListener('DOMContentLoaded',imageShowcase);
+document.addEventListener("DOMContentLoaded", readMore);
+document.addEventListener("DOMContentLoaded", imageShowcase);
